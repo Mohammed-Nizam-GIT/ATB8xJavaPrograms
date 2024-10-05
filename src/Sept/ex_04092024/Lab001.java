fc6e0c0c6e953559c39acfbe9e7de64be0d50c05
intel <nizamnrp805@gmail.com>
2024-10-05 13:41:03 +0530

    commit2

--- src/Sept/ex_04092024/Lab001.java
+++ src/Sept/ex_04092024/Lab001.java
@@ -4,5 +4,8 @@ public class Lab001 {
         public static void main(String[] args) {
             System.out.println("Hello World002");
             System.out.println("Hello World003");
+            System.out.println("Hello World004");
         }
 }
+
+
